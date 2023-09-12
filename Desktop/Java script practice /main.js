3daeae3148b6ff443618565e41df14773e92fedd
Selam Ararsa <selamkedir@icloud.com>
2023-09-12 09:52:58 +0100

    Ticket 5 a

---
+++ Desktop/Java script practice /main.js
@@ -0,0 +1,9 @@
+let line1 = 'Once upon a time ';
+let line2 = 'there was a big duck ';
+let line3 = 'quacking around the city';
+let ourStory = line1 + line2 + line3;
+console.log(ourStory);
+let num = 6;
+if (num > 2){
+    console.log('Thats correct.')
+}
