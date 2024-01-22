f9c88d74321f225d0b0d3f940e43b0a68722ae2f
Selam Ararsa <selamkedir@icloud.com>
2024-01-22 09:39:09 +0000

    added a favicon

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -29,13 +29,7 @@ module.exports = {
         'light-4': '#5C5C7B',
       },
       spacing: {
-        '1': '0.25rem',
-        '2': '0.5rem',
-        '3': '0.75rem',
-        '4': '1rem',
-        '5': '1.25rem',
-        // Add more custom spacing values as needed
-      },
+        
         18: '4.5rem',
         15: '3.75rem',
         12: '3rem',
