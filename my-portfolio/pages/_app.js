9b892f413010b1a0a6ffbddc8f43e39f7bb54006
Selam Ararsa <selamkedir@icloud.com>
2024-02-04 19:50:08 +0000

    next/font issue - not fixed

--- my-portfolio/pages/_app.js
+++ my-portfolio/pages/_app.js
@@ -1,11 +1,5 @@
 import '@/styles/globals.css';
-import { Roboto } from 'next/font/google';
 
-const roboto = Roboto({
-  subsets: ['latin'],
-  weights: [400, 700],
-  display: 'swap',
-});
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
