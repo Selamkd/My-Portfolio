ba6d392b685de9c1b26d39f5a145d6eae333a9c4
Selam Ararsa <selamkedir@icloud.com>
2024-02-05 22:55:45 +0000

    font issue resolved

--- my-portfolio/pages/index.jsx
+++ my-portfolio/pages/index.jsx
@@ -1,11 +1,7 @@
 import Image from 'next/image';
 import { Roboto } from 'next/font/google';
 import Portfolio from '../components/Portfolio';
-const roboto = Roboto({
-  subsets: ['latin'],
-  weights: [400, 700],
-  display: 'swap',
-});
+
 export default function Home() {
   return (
     <main className="flex flex-col h-screen">
