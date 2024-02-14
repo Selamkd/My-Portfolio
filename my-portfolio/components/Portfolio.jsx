d03442f70a1c67c377e8ab751c0a598e4d6f0e80
Selam Ararsa <selamkedir@icloud.com>
2024-02-14 18:44:40 +0000

    add framer animations for landing page

--- my-portfolio/components/Portfolio.jsx
+++ my-portfolio/components/Portfolio.jsx
@@ -4,8 +4,9 @@ import heading from '../public/portfolio.png';
 import Avatar from '../public/avatar-pic.png';
 import Arrow from '../public/Arrow-button.png';
 import Icon from '../public/Name-tag.png';
-import Heart from '../public/heart.png';
+import { ReactComponent as Heart } from '../public/Heart.svg';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 const Projects = () => {
   return (
     // <section className=" w-[100%] h-screen flex flex-col items-center ">
@@ -30,15 +31,34 @@ const Projects = () => {
     //   </Link>
     // </section>
     <section className="w-[100%] flex flex-col h-screen justify-center items-center >">
-      <div className="flex items-center justify-center">
+      <motion.div
+        whileTap={{
+          x: -120,
+          y: -171,
+        }}
+        className="flex items-center justify-center"
+      >
         <h1 className="text-9xl text-purple">Selam Ararsa</h1>
         <div className="mb-20">
           <Image src={Icon} alt="icon" width={100} height={100} />
         </div>
-      </div>
+      </motion.div>
       <h3 className="text-2xl text-blue-100">Full-stack developer</h3>
-
-      <Image src={Heart} alt="avatar pic" width={300} height={80} />
+      {/* <motion.svg
+        drag
+        dragConstraints={{
+          top: -50,
+          left: -50,
+          right: 50,
+          bottom: 50,
+        }}
+        width={300}
+        height={80}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 300 80" // set the viewBox to the size of the heart
+      > */}
+      {/* <Heart /> */}
+      {/* </motion.svg> */}
     </section>
   );
 };
