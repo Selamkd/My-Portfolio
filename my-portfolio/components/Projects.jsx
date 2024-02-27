40868455548e2aeaeb66b056fcd310adf4b31eed
Selam Ararsa <selamkedir@icloud.com>
2024-02-27 21:17:32 +0000

    project page layout/ styling done

--- my-portfolio/components/Projects.jsx
+++ my-portfolio/components/Projects.jsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faJs, faReact } from '@fortawesome/free-brands-svg-icons';
 import { motion } from 'framer-motion';
+import { SiNextdotjs } from 'react-icons/si';
+import { RiSupabaseLine } from 'react-icons/ri';
 import Icon from '../public/sparkle.png';
 
 const Projects = () => {
@@ -63,27 +65,55 @@ const Projects = () => {
         >
           <div className="flex__item flex__item--left">
             <div className="flex__content">
-              <p className="icons">
-                <FontAwesomeIcon icon={faJs} size="xl" />
-                <FontAwesomeIcon icon={faReact} size="xl" />
-              </p>
-              <div className="flex items-center justify-center">
-                <h1 className="text--big mr-20">Attract</h1>
+              <div className="flex items-center justify-start">
+                <h1 className="text--big ">Attract</h1>
+                <Image
+                  src={Icon}
+                  className="align-center ml-10 mb-10"
+                  alt="arrow"
+                  width={60}
+                  height={40}
+                />
               </div>
 
               <p className="text--normal">
-                This project is a web app that uses a REST API to provide League
-                of Legends fans with a distinctive quiz experience. Regardless
-                of your level of experience as a summoner or where you are on
-                the rift, this quiz will help you identify your ideal champion
-                based on your preferences and playstyle.
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos
+                <span>
+                  {' '}
+                  qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+                  est, qui dolorem ipsum quia dolor sit amet, consectetur
+                </span>
+                adipisci velit, sed quia non numquam eius modi tempora incidunt
+                ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim
+                ad minima veniam.
               </p>
               <div className="mt-3 ">
-                <button className="btn">Live</button>
+                <button className="btn mr-2">Live</button>
                 <button className="btn ">Repo</button>
               </div>
+              <div className="flex items-center justify-start ml-4 mt-5">
+                <FontAwesomeIcon icon={faJs} className="text-4xl mr-4 " />
+                <FontAwesomeIcon icon={faReact} className="text-4xl mr-4 " />
+                <svg
+                  class="w-10 h-10 text-4xl mr-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  id="tailwind-icon"
+                >
+                  <path d="M11.8 5.7A4.8 4.8 0 0 0 7 10a3.4 3.4 0 0 1 2.7-1.7c1.7 0 3 2 3.8 2.6a5.7 5.7 0 0 0 5.4 1c2-.7 2.9-3 3.1-4-1 1.4-2.4 2.2-4.3 1.2-1.2-.6-2.1-3.4-6-3.3Zm-5 6.3A4.8 4.8 0 0 0 2 16.2a3.4 3.4 0 0 1 2.7-1.7c1.7 0 3 2 3.8 2.6a5.7 5.7 0 0 0 5.4.9c2-.7 3-2.9 3.1-4-1 1.4-2.4 2.3-4.2 1.3-1.3-.7-2.2-3.4-6-3.3Z" />
+                </svg>
+                <SiNextdotjs className="text-4xl mr-5 h-8 w-8" />
+                <RiSupabaseLine className="text-4xl mr-5 h-8 w-8" />
+              </div>
             </div>
-            <p className="text__background">00</p>
+            {/* <p className="text__background">00</p> */}
             <motion.div
               animate={{ scale: 1, opacity: 1 }}
               id="arrow-container"
@@ -112,21 +142,50 @@ const Projects = () => {
         >
           <div className="flex__item flex__item--left">
             <div className="flex__content">
-              {/* <p className="text--sub">PROJECTS</p> */}
-              <h1 className="text--big">ChampSelect</h1>
+              <div className="flex items-center justify-start">
+                <h1 className="text--big ">Champselect</h1>
+                <Image
+                  src={Icon}
+                  className="align-center ml-10 mb-10"
+                  alt="arrow"
+                  width={60}
+                  height={40}
+                />
+              </div>
               <p className="text--normal">
-                This project is a web app that uses a REST API to provide League
-                of Legends fans with a distinctive quiz experience. Regardless
-                of your level of experience as a summoner or where you are on
-                the rift, this quiz will help you identify your ideal champion
-                based on your preferences and playstyle.
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+                sit amet, consectetur, adipisci velit, sed quia non numquam eius
+                modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                voluptatem. Ut enim ad minima veniam.
               </p>
               <div className="mt-3 ">
-                <button className="btn">Live</button>
+                <button className="btn mr-2">Live</button>
                 <button className="btn ">Repo</button>
               </div>
+              <div className="flex items-center justify-start ml-4 mt-5">
+                <FontAwesomeIcon icon={faJs} className="text-4xl mr-4 " />
+                <FontAwesomeIcon icon={faReact} className="text-4xl mr-4 " />
+                <svg
+                  class="w-10 h-10 text-4xl mr-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  id="tailwind-icon"
+                >
+                  <path d="M11.8 5.7A4.8 4.8 0 0 0 7 10a3.4 3.4 0 0 1 2.7-1.7c1.7 0 3 2 3.8 2.6a5.7 5.7 0 0 0 5.4 1c2-.7 2.9-3 3.1-4-1 1.4-2.4 2.2-4.3 1.2-1.2-.6-2.1-3.4-6-3.3Zm-5 6.3A4.8 4.8 0 0 0 2 16.2a3.4 3.4 0 0 1 2.7-1.7c1.7 0 3 2 3.8 2.6a5.7 5.7 0 0 0 5.4.9c2-.7 3-2.9 3.1-4-1 1.4-2.4 2.3-4.2 1.3-1.3-.7-2.2-3.4-6-3.3Z" />
+                </svg>
+                <SiNextdotjs className="text-4xl mr-5 h-8 w-8" />
+                <RiSupabaseLine className="text-4xl mr-5 h-8 w-8" />
+              </div>
             </div>
-            <p className="text__background">02</p>
+
             <motion.div
               animate={{ scale: 1, opacity: 1 }}
               id="arrow-container"
@@ -155,9 +214,17 @@ const Projects = () => {
         >
           <div className="flex__item flex__item--left">
             <div className="flex__content">
-              <p className="text--sub">Not sure</p>
-              <h1 className="text--big">Hackafun</h1>
-              <p className="text--normal">
+              <div className="flex items-center justify-start">
+                <h1 className="text--big ">Hackafun</h1>
+                <Image
+                  src={Icon}
+                  className="align-center ml-8 mb-10"
+                  alt="arrow"
+                  width={60}
+                  height={40}
+                />
+              </div>
+              <p className="text--normal ">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                 accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
                 quae ab illo inventore veritatis et quasi architecto beatae
@@ -170,11 +237,27 @@ const Projects = () => {
                 voluptatem. Ut enim ad minima veniam.
               </p>
               <div className="mt-3 ">
-                <button className="btn">Live</button>
+                <button className="btn mr-2">Live</button>
                 <button className="btn ">Repo</button>
               </div>
+              <div className="flex items-center justify-start ml-4 mt-5">
+                <FontAwesomeIcon icon={faJs} className="text-4xl mr-4 " />
+                <FontAwesomeIcon icon={faReact} className="text-4xl mr-4 " />
+                <svg
+                  class="w-10 h-10 text-4xl mr-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  id="tailwind-icon"
+                >
+                  <path d="M11.8 5.7A4.8 4.8 0 0 0 7 10a3.4 3.4 0 0 1 2.7-1.7c1.7 0 3 2 3.8 2.6a5.7 5.7 0 0 0 5.4 1c2-.7 2.9-3 3.1-4-1 1.4-2.4 2.2-4.3 1.2-1.2-.6-2.1-3.4-6-3.3Zm-5 6.3A4.8 4.8 0 0 0 2 16.2a3.4 3.4 0 0 1 2.7-1.7c1.7 0 3 2 3.8 2.6a5.7 5.7 0 0 0 5.4.9c2-.7 3-2.9 3.1-4-1 1.4-2.4 2.3-4.2 1.3-1.3-.7-2.2-3.4-6-3.3Z" />
+                </svg>
+                <SiNextdotjs className="text-4xl mr-5 h-8 w-8" />
+                <RiSupabaseLine className="text-4xl mr-5 h-8 w-8" />
+              </div>
             </div>
-            <p className="text__background">03</p>
+
             <motion.div
               animate={{ scale: 1, opacity: 1 }}
               id="arrow-container"
