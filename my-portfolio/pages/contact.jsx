7a934dfbf4f39e4e2682820cd76c3311f65d579a
Selam Ararsa <selamkedir@icloud.com>
2024-03-05 20:38:48 +0000

    left comments

--- my-portfolio/pages/contact.jsx
+++ my-portfolio/pages/contact.jsx
@@ -9,3 +9,7 @@ export default function Contact() {
     </main>
   );
 }
+/*Taking a day off to rest after sending out an application. Leaving this message here to remind 
+myself that I am loved and appreciated. Also to remind Selam that you inspire me to challenge and 
+push myself and i would do anything to have even 1% of the talents that you possess.
+*
