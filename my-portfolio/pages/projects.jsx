805e1cdbce36f1691f018ad302e22c9567df3cc6
Selam Ararsa <selamkedir@icloud.com>
2024-03-14 23:17:56 +0000

    cleaned up code

--- my-portfolio/pages/projects.jsx
+++ my-portfolio/pages/projects.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 export default function Projects() {
   return (
     <main className="flex flex-col h-screen">
-      <div className="fixed top-2 z-20 align-center ml-15">
+      <div className="fixed top-2 z-80 align-center ml-15">
         <Header />
       </div>
 
