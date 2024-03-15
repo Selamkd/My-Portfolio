aeac54bf02d04f29aafebd7ff80df27b27330f11
Selam Ararsa <selamkedir@icloud.com>
2024-03-15 23:20:21 +0000

    projects page issue fixed

--- my-portfolio/pages/projects.jsx
+++ my-portfolio/pages/projects.jsx
@@ -1,6 +1,7 @@
 import ProjectsPage from '../components/Projects';
 import Header from '../components/Header';
 export default function Projects() {
+  // Render the ProjectsPage component
   return (
     <main className="flex flex-col h-screen">
       <div className="fixed top-2 z-80 align-center ml-15">
