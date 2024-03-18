0238a451c9222d66c44cfab6e87c76229cd4a42b
Selam Ararsa <selamkedir@icloud.com>
2024-03-18 20:08:45 +0000

    n

--- my-portfolio/components/SpotifyModal.jsx
+++ my-portfolio/components/SpotifyModal.jsx
@@ -18,7 +18,7 @@ const ModalContent = (props) => {
   return (
     <>
       <section>
-        <div className="relative flex justify-center z-10 items-center">
+        <div className="relative flex justify-center z-10 items-start">
           <div
             id="menu"
             className={`w-full h-full bg-gray-900 z-900 bg-opacity-80 top-0 fixed sticky-0 ${
@@ -27,7 +27,7 @@ const ModalContent = (props) => {
           >
             {!start ? (
               <div className="2xl:container   2xl:mx-auto py-48 l px-4 md:px-28 flex justify-center  lg:py-28 items-center">
-                <div className="w-96 lg:h-600  md:w-auto  bg-gray-800 relative flex flex-col  justify-center items-center  py-16 px-4 md:px-24 xl:py-24 xl:px-36">
+                <div className="w-96 lg:h-500  md:w-auto  bg-gray-800 relative flex flex-col  justify-center items-center  py-16 px-4 md:px-24 xl:py-24 xl:px-36">
                   <div role="banner ">
                     <SlSocialSpotify size={123} className="text-[#1DB954]" />
                   </div>
