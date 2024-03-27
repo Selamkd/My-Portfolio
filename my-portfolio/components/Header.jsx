165aa1aad3e9157ab2070cb8bb83d47622dd1eaf
Selam Ararsa <selamkedir@icloud.com>
2024-03-27 20:58:50 +0000

    modal issue fixed

--- my-portfolio/components/Header.jsx
+++ my-portfolio/components/Header.jsx
@@ -15,47 +15,50 @@ const Header = () => {
     setModal(false);
   };
   return (
-    <header className="top-0 m-0 text--small sm:top-4 rounded-none sm:rounded-full border-b sm:border dark:bg-blackish/30 border-gray-800/50 dark:border-gray-400/10 p-3 z-50 backdrop-blur-md w-full m-auto max-w-3xl flex justify-between items-center sticky">
-      <nav className="w-full flex justify-around items-center">
-        <div className="flex items-center space-x-8">
-          <div id="links" class="  hidden items-center sm:flex">
-            <Link
-              class="flex  items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
-              href="/ "
-            >
-              Home
-            </Link>
-            <Link
-              class="flex items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
-              href="/about"
-            >
-              About Me
-            </Link>
-            <Link
-              class="flex items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
-              href="/projects"
-            >
-              Projects
-            </Link>
-            <Link
-              class="flex items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
-              href="/contact"
-            >
-              Contact Me
-            </Link>
-          </div>
+    <>
+      {/* prompt to start the modal */}
+      <header className="top-0 text--small sm:top-4 rounded-none sm:rounded-full border-b sm:border dark:bg-blackish/30 border-gray-800/50 dark:border-gray-400/10 p-3 z-50 backdrop-blur-md w-full m-auto max-w-3xl flex justify-between items-center sticky">
+        <nav className="w-full flex justify-around items-center">
+          <div className="flex items-center space-x-8">
+            <div id="links" class="  hidden items-center sm:flex">
+              <Link
+                class="flex  items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
+                href="/ "
+              >
+                Home
+              </Link>
+              <Link
+                class="flex items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
+                href="/about"
+              >
+                About Me
+              </Link>
+              <Link
+                class="flex items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
+                href="/projects"
+              >
+                Projects
+              </Link>
+              <Link
+                class="flex items-center justify-center rounded-full transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-draplin text-[13px] tracking-[0.2px] leading-none cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-white/5 text-gray-800 dark:text-gray-400 font-medium px-3 py-2  w-fit"
+                href="/contact"
+              >
+                Contact Me
+              </Link>
+            </div>
 
-          <TbMusicHeart
-            onClick={showModal}
-            className="h-4 w-4 text-gray-600 dark:text-gray-400 hover:text-purple"
-          />
-          <a href="/Selam Ararsa.pdf" download>
-            <TbFileDownload className="h-5 w-5 text-gray-600 dark:text-gray-400 hover:text-purple" />
-          </a>
-        </div>
-      </nav>
+            <TbMusicHeart
+              onClick={showModal}
+              className="h-4 w-4 text-gray-600 dark:text-gray-400 hover:text-purple"
+            />
+            <a href="/Selam Ararsa.pdf" download>
+              <TbFileDownload className="h-5 w-5 text-gray-600 dark:text-gray-400 hover:text-purple" />
+            </a>
+          </div>
+        </nav>
+      </header>
       <SpotifyModal modal={modal} closeModal={closeModal} />
-    </header>
+    </>
   );
 };
 
