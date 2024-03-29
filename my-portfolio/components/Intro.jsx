89c19ee58059bdfe5cc52d85cb70934b258c3de9
Selam Ararsa <selamkedir@icloud.com>
2024-03-29 10:27:40 +0000

    intro page scroll issue fixed

--- my-portfolio/components/Intro.jsx
+++ my-portfolio/components/Intro.jsx
@@ -7,7 +7,7 @@ import Github from '../public/Github.png';
 import Image from 'next/image';
 import Me from '../public/IMG_1230.jpeg';
 import Link from 'next/link';
-import yellowArrow from '../public/yellow-arrow.png';
+import yellowArrow from '../public/pic-arrow.png';
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Twinkle_Star } from 'next/font/google';
@@ -120,75 +120,76 @@ const Intro = () => {
           </div>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:items-center w-full h-screen">
-          {/* Image and social links */}
-          <div className="absolute flex justify-between top-0 w-[100%] ">
-            <div className="absolute top-0 left-0">
-              <Image src={Bluewave} alt="avatar pic" width={140} height={130} />
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:items-center w-full h-screen">
+            {/* Image and social links */}
+            <div className="absolute flex justify-between top-0 w-[100%] ">
+              <div className="absolute top-0 left-0">
+                <Image
+                  src={Bluewave}
+                  alt="avatar pic"
+                  width={140}
+                  height={130}
+                />
+              </div>
+
+              <div className="absolute top-0 right-0">
+                <Image
+                  src={Cherry}
+                  className="justify-end "
+                  alt="avatar pic"
+                  width={140}
+                  height={130}
+                />
+              </div>
             </div>
 
-            <div className="absolute top-0 right-0">
-              <Image
-                src={Cherry}
-                className="justify-end "
-                alt="avatar pic"
-                width={140}
-                height={130}
-              />
+            <div className="md:flex md:flex-col  md:items-center md:justify-center">
+              <div className="flex justify-between  align-center w-full mb-4"></div>
+              <div className="mb-4 z-1 ">
+                <Image
+                  src={Me}
+                  className="rounded-lg z-100 ar"
+                  alt="avatar pic"
+                  width={320}
+                  height={200}
+                />
+              </div>
+              <div className="flex items-center justify-center gap-8">
+                <Link href="https://github.com/Selamkd">
+                  <Image src={Github} alt="avatar pic" width={40} height={30} />
+                </Link>
+                <Link href="https://www.linkedin.com/in/selam-ararsa-39937126b/">
+                  <Image
+                    src={Linkeden}
+                    alt="avatar pic"
+                    width={40}
+                    height={30}
+                  />
+                </Link>
+                <Link href="https://www.linkedin.com/in/selam-ararsa-39937126b/">
+                  <Image src={Mail} alt="avatar pic" width={40} height={30} />
+                </Link>
+              </div>
             </div>
-          </div>
+            {/* Text and Arrow */}
+            <div className="md:px-3 w-[80%] ">
+              <h2>Hello</h2>
+              <p className="text--normal ">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+                sit amet, consectetur, adipisci velit, sed quia non numquam eius
+                modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                voluptatem. Ut enim ad minima veniam.
+              </p>
 
-          <div className="md:flex md:flex-col  md:items-center md:justify-center">
-            <div className="flex justify-between  align-center w-full mb-4"></div>
-            <div className="mb-4 z-1 ">
-              <Image
-                src={Me}
-                className="rounded-lg z-100 ar"
-                alt="avatar pic"
-                width={320}
-                height={200}
-              />
-            </div>
-            <div className="flex items-center justify-center gap-8">
-              <Link href="https://github.com/Selamkd">
-                <Image src={Github} alt="avatar pic" width={40} height={30} />
-              </Link>
-              <Link href="https://www.linkedin.com/in/selam-ararsa-39937126b/">
-                <Image src={Linkeden} alt="avatar pic" width={40} height={30} />
-              </Link>
-              <Link href="https://www.linkedin.com/in/selam-ararsa-39937126b/">
-                <Image src={Mail} alt="avatar pic" width={40} height={30} />
-              </Link>
-            </div>
-          </div>
-          {/* Text and Arrow */}
-          <div className="md:px-3 w-[80%] ">
-            <h1
-              id="greet"
-              className="text--big  text-3xl!important text-center mb-10"
-            >
-              Hello
-            </h1>
-            <p className="text--normal ">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-              velit, sed quia non numquam eius modi tempora incidunt ut labore
-              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-              veniam.
-            </p>
-
-            {/* Arrow */}
-            <motion.div
-              initial="hidden"
-              animate={{ scale: 1, opacity: 1 }}
-              className="absolute  bottom-0 right-45"
-            >
-              <Link href="/">
+              {/* Arrow */}
+              <Link href="/projects">
                 <svg className="arrows">
                   <path
                     strokeLinecap="round"
@@ -197,20 +198,21 @@ const Intro = () => {
                   ></path>
                 </svg>
               </Link>
-            </motion.div>
+            </div>
+
+            <div className="absolute bottom-20 left-0 flex justify-center mr-36  w-[100%]">
+              <Image
+                src={yellowArrow}
+                alt="avatar pic"
+                width={220}
+                height={220}
+              />
+            </div>
           </div>
-          <div className="absolute bottom-0 right-0 flex justify-end  w-[100%]">
-            <Image src={Pinkwave} alt="avatar pic" width={240} height={230} />
+          <div className="flex w-full justify-end fixed bottom-10">
+            <Image src={Pinkwave} alt="avatar pic" width={220} height={210} />
           </div>
-          <div className="absolute bottom-0 right-0 flex justify-center mr-36  w-[100%]">
-            <Image
-              src={yellowArrow}
-              alt="avatar pic"
-              width={240}
-              height={230}
-            />
-          </div>
-        </div>
+        </>
       )}
     </>
   );
