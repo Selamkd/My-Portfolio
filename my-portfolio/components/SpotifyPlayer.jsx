255ddb38410b9b02dbab142a7e0befac957562f1
Selam Ararsa <selamkedir@icloud.com>
2024-04-06 23:29:55 +0100

    repeat issue

--- my-portfolio/components/SpotifyPlayer.jsx
+++ my-portfolio/components/SpotifyPlayer.jsx
@@ -7,40 +7,55 @@ import { CiPlay1 } from 'react-icons/ci';
 import { GrNext } from 'react-icons/gr';
 import { GrPrevious } from 'react-icons/gr';
 import { MdOutlineRestartAlt } from 'react-icons/md';
+import { useEffect } from 'react';
 const SpotifyPlayer = (props) => {
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
-
-  const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
   const [isPlaying, setIsPlaying] = useState(true);
+  const [shuffledTrackIds, setShuffledTrackIds] = useState([]);
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
 
   const playNextTrack = () => {
     setCurrentTrackIndex((prevIndex) => {
       const nextIndex = prevIndex + 1;
-      return nextIndex < filteredTracks.length ? nextIndex : 0;
+
+      if (nextIndex >= filteredTracks.length) {
+        const reshuffledTrackIds = shuffleArray(trackIds);
+        setShuffledTrackIds(reshuffledTrackIds);
+        return 0;
+      }
+      return nextIndex;
     });
   };
 
   const playPreviousTrack = () => {
-    setCurrentTrackIndex(
-      (prevIndex) =>
-        (prevIndex - 1 + shuffledTrackIds.length) % shuffledTrackIds.length
-    );
+    setCurrentTrackIndex((prevIndex) => {
+      const nextIndex =
+        (prevIndex - 1 + filteredTracks.length) % filteredTracks.length;
+      return nextIndex;
+    });
   };
 
-  const shuffledTrackIds = shuffleArray(trackIds);
+  useEffect(() => {
+    const shuffledTrackIds = shuffleArray(trackIds);
+    setShuffledTrackIds(shuffledTrackIds);
+  }, []);
 
   const filteredTracks = shuffledTrackIds.filter(
     (track) => track.moodTag === props.mood
   );
-
-  const spotifyEmbedUrl = `https://open.spotify.com/embed/track/${filteredTracks[currentTrackIndex]?.trackId}`;
+  const currentTrack = filteredTracks[currentTrackIndex];
+  const spotifyEmbedUrl = `https://open.spotify.com/embed/track/${currentTrack?.trackId}`;
 
   return (
     <div className="spotify-player fixed left-0 top-0 flex flex-col items-center justify-center pointer-events-auto mt-[8%] w-full h-[60%] bg-gray-900 z-900 py-8 bg-opacity-80 sticky-0">
